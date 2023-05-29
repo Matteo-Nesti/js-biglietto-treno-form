c6c6514f67eb070cd99f4a8444af0cf96452df89
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-05-29 18:26:25 +0200

    add active and remove class for ticket

--- js/script.js
+++ js/script.js
@@ -12,44 +12,54 @@ const inputKm = document.getElementById('input-km');
 const button = document.getElementById('button');
 const minor = document.getElementById('minor')
 const over65 = document.getElementById('over-65')
-let discount = 0;
+let discount = null;
 
 // evento per richiamare gli input
 button.addEventListener('click', function(){ 
-
+    //recupero value input name
     const name = inputName.value.trim();
     console.log(name + ' nome')
     userName.innerText = name
-    
-    const km = inputKm.value;
-    console.log(km + ' km')
-    userKm.innerText = km + ' chilometri'
 
-    const age = inputAge.value;
-    console.log(age + ' fascia eta')
-    userAge.innerText = age
-
-    
-    
-    const ticketTotalPrice = (km * ticketPriceForKm).toFixed(2);
-    console.log(ticketTotalPrice + ' prezzo biglietto totale')
-
-    const junior = minor.value
-    console.log(junior + ' value minor')
-    const senior = over65.value
-    console.log(senior + ' value over65')
-
-    if (age===junior) discount = 20
-    else if (age===senior) discount = 40
-
-    if (discount){
-        const discountTicket = (ticketTotalPrice - (ticketTotalPrice * discount / 100)).toFixed(2) 
-        console.log(discountTicket + ' discount' + discount + '%')
-        
-        trainPrice.innerHTML = ` ${discountTicket} € invece di <del>${ticketTotalPrice}</del> € `
+// verifica 
+    if (name == '') {
+        alert('inserire un nome')
     }
-    else {
-        trainPrice.innerHTML = ticketTotalPrice + '€'
+    else { 
+// rendere visibile il biglietto 
+
+        const ticketContainer = document.getElementById('ticket-container')
+        ticketContainer.classList.remove('deactive')
+        ticketContainer.classList.add('active')
+
+ // altrimenti recupero km e eta`       
+        const km = inputKm.value;
+        console.log(km + ' km')
+        userKm.innerText = km + ' chilometri'
+    
+        const age = inputAge.value;
+        console.log(age + ' fascia eta')
+        userAge.innerText = age
+// recupero value della scelta per la fascia d'eta`
+        const junior = minor.value
+        console.log(junior + ' value minor')
+        const senior = over65.value
+        console.log(senior + ' value over65')
+// calcolo prezzo biglietto intero
+        const ticketTotalPrice = (km * ticketPriceForKm).toFixed(2);
+       
+        if (age===junior) discount = 20
+        else if (age===senior) discount = 40
+    
+        if (discount){
+            const discountTicket = (ticketTotalPrice - (ticketTotalPrice * discount / 100)).toFixed(2) 
+            console.log(discountTicket + ' discount' + discount + ' %')
+            trainPrice.innerHTML = ` ${discountTicket} € invece di <del>${ticketTotalPrice}</del> € `
+        }
+        else {
+            trainPrice.innerHTML = ticketTotalPrice + '€'
+            console.log(ticketTotalPrice + ' prezzo biglietto totale')
+        }
     }
 })
 
